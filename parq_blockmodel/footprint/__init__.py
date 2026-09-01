092a7351267c43c42cadc9c48762aa8d6c7b3d66
Greg <11791585+elphick@users.noreply.github.com>
2026-09-01 19:37:08 +0800

    Closes #135 extent footprints

--- parq_blockmodel/footprint/__init__.py
+++ parq_blockmodel/footprint/__init__.py
@@ -1,3 +1,4 @@
 from .categorical import to_categorical_geodataframe
+from .extents import to_footprint_geodataframe
 
-__all__ = ["to_categorical_geodataframe"]
+__all__ = ["to_categorical_geodataframe", "to_footprint_geodataframe"]
